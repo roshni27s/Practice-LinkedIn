ff40a884525f529122600faa14e4667dbf5090bf
Roshni Singh <roshni.rx@gmail.com>
2022-10-17 23:34:24 +0530

    header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,29 @@ const Header = (props) => {
     return (
         <Container>
             <Content>
-                Headervv
+                <Logo>
+                    <a href="/home">
+                        <img src="/images/home-logo.svg" alt=""/>
+                    </a>
+                </Logo>
+                <Search>
+                    <div>
+                        <input type="text" placeholder="Search"/>
+                    </div>
+                    <SearchIcon>
+                        <img src="/images/search-icon.svg" alt=""/>
+                    </SearchIcon>
+                </Search>
+                <Nav>
+                    <NavListWrap>
+                        <NavList>
+                            <a>
+                                <img src="/images/nav-home.svg" alt=""/>
+                                <span>Home</span>
+                            </a>
+                        </NavList>
+                    </NavListWrap>
+                </Nav>
             </Content>
         </Container>
     )
@@ -26,6 +48,95 @@ align-items: center;
 margin: 0 auto;
 min-height: 100%;
 max-width: 1128px;
+`
+const Logo = styled.span`
+margin-right: 8px;
+font-size: 0px;
+`
+const Search = styled.div`
+opacity: 1;
+flex-grow: 1;
+position: relative;
+& > div {
+    max-width: 280px;
+    input {
+        border: none;
+        box-shadow: none;
+        background-color: #eef3f8;
+        border-radius: 2px;
+        color: rgba(0, 0, 0, 0.9);
+        width: 218px;
+        padding: 0 8px 0 40px;
+        line-height: 1.75;
+        font-weight: 400;
+        font-size: 14px;
+        height: 34px;
+        border-color: #dce6f1;
+        vertical-align: text-top;
+    }
+}
+`
+const SearchIcon = styled.div` 
+width: 40px;
+position: absolute;
+z-index: 1;
+top: 10px;
+left: 2px;
+border-radius: 0 2px 2px 0;
+margin: 0;
+pointer-events: none;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+const Nav = styled.nav`
+margin-left: auto;
+display: block;
+@media (max-width: 768px) {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background: white;
+    width: 100%;
+}
+`
+const NavListWrap = styled.ul`
+display: flex;
+flex-wrap: nowrap;
+list-style-type: none;
+`
+const NavList = styled.li`
+display: flex;
+align-items: center;
+a{
+    align-items: center;
+    background: transparent;
+    display: flex;
+    flex-direction: column;
+    font-size: 12px;
+    font-weight: 400;
+    justify-content: center;
+    line-height: 1.5;
+    min-height: 42px;
+    min-width: 80px;
+    position: relative;
+    text-decoration: none;
+    span{
+        color: rgba(0, 0, 0, 0.6);
+        display: flex;
+        align-items: center;
+    }
+    @media (max-width: 768px) {
+        min-width: 70px;
+    }
+}
+&:hover, &:active{
+    a{
+        span{
+            color: rgba(0, 0, 0, 0.9);
+        }
+    }
+}
 `
 
 export default Header;
