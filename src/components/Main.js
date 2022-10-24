134e14db3844bf0c7a59115ac0bd31f17bf2882e
Roshni Singh <roshni.rx@gmail.com>
2022-10-24 18:27:55 +0530

    post article in main

--- src/components/Main.js
+++ src/components/Main.js
@@ -27,6 +27,47 @@ const Main = (props) => {
                 </button>
             </div>
             </ShareBox>
+            <div>
+                <Article>
+                    <SharedActor>
+                        <a>
+                            <img src="/images/user.svg" alt=""/>
+                            <div>
+                                <span>Title</span>
+                                <span>Info</span>
+                                <span>Date</span>
+                            </div>
+                        </a>
+                        <button>
+                            <img src="/images/ellipsis.png" alt=""/>
+                        </button>
+                    </SharedActor>
+                    <Description>Description</Description>
+                    <SharedImg>
+                        <a>
+                            <img src="/images/shared-img.png" alt=""/>
+                        </a>
+                    </SharedImg>
+                    <SocialCounts>
+                        <li>
+                            <button>
+                                <img src="/images/like-icon.png" alt=""/>
+                                <img src="/images/clap-icon.png" alt=""/>
+                                <span>75</span>
+                            </button>
+                        </li>
+                        <li>
+                            <a>
+                                2 comments
+                            </a>
+                        </li>
+                    </SocialCounts>
+                    <button>
+                        <img src="/images/like-icon.png" alt=""/>
+                        <span>Like</span>
+                    </button>
+                </Article>
+            </div>
         </Container>
     )
 }
@@ -92,7 +133,102 @@ div{
                 margin: 0 4px 0 -2px;
                 width: 28px;
             }
+            span{
+                color: #70b5f9;
+            }
         }
+    }
+}
+`
+const Article = styled(CommonCard)`
+padding: 0;
+margin: 0 0 8px;
+overflow: visible;
+`
+const SharedActor = styled.div`
+padding-right: 40px;
+flex-wrap: nowrap;
+padding: 12px 16px 0;
+margin-bottom: 8px;
+align-items: center;
+display: flex;
+a{
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+    img{
+        width: 48px;
+        height: 48px;
+    }
+    & > div {
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+        flex-basis: 0;
+        margin-left: 8px;
+        overflow: hidden;
+        span{
+            text-align: left;
+            &:first-child{
+                font-size: 14px;
+                font-weight: 700;
+                color: rgba(0, 0, 0, 1);
+            }
+            &:nth-child(n+1){
+                font-size: 12px;
+                color: rgba(0, 0, 0, 0.6);
+            }
+        }
+    }
+}
+button{
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+    img{
+        width: 30px;
+    }
+}
+`
+const Description = styled.div`
+padding: 0 16px;
+overflow: hidden;
+color: rgba(0, 0, 0, 0.9);
+font-size: 14px;
+text-align: left;
+`
+
+const SharedImg = styled.div`
+margin-top: 8px;
+width: 100%;
+display: block;
+position: relative;
+background-color: #f9fafb;
+img{
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+}
+`
+const SocialCounts = styled.ul`
+line-height: 1.3;
+display: flex;
+align-items: flex-start;
+overflow: auto;
+margin: 0 16px;
+padding: 8px 0;
+border-bottom: 1px solid #e9e5df;
+list-style: none;
+li{
+    margin-right: 5px;
+    font-size: 12px;
+    button{
+        display: flex;
     }
 }
 `
